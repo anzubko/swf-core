2e50c01cdc6d0fecab2689b44ef05d52a3ca4c41
Andrian Zubko <ondr@mail.ru>
2024-10-11 21:11:12 +0300

    dev

--- src/SimpleMailer.php
+++ src/SimpleMailer.php
@@ -6,45 +6,44 @@ use InvalidArgumentException;
 use PHPMailer\PHPMailer\Exception AS PHPMailerException;
 use PHPMailer\PHPMailer\PHPMailer;
 use RuntimeException;
+use function count;
 
 final class SimpleMailer
 {
     private PHPMailer $mailer;
 
-    private bool $strict;
-
     /**
      * @param bool $enabled Is mailer enabled or not.
-     * @param array{string, string|null}|string|null $sender Default sender: 'EMAIL' or array('EMAIL'[, 'NAME']) or null
-     * @param array<array{string, string|null}>|null $recipients Override recipients: ?array('EMAIL' or array('EMAIL'[, 'NAME']), ...)
-     * @param array<array{string, string|null}>|null $replies Default replies: ?array('EMAIL' or array('EMAIL'[, 'NAME']), ...)
+     * @param array{string, string|null}|string|null $sender Default sender (email with optional name).
+     * @param array<array{string, string|null}>|null $recipients Override recipients.
+     * @param array<array{string, string|null}>|null $replies Default replies.
      * @param bool $strict Adding wrong recipients, cc, replies and errors on sending will throw exceptions.
      *
      * @throws InvalidArgumentException
      */
     public function __construct(
         private readonly bool $enabled = true,
+        private readonly bool $strict = false,
         array|string|null $sender = null,
         ?array $recipients = null,
         ?array $replies = null,
-        bool $strict = false,
     ) {
         $this->mailer = new PHPMailer(true);
         $this->mailer->CharSet = PHPMailer::CHARSET_UTF8;
-
-        $this->strict = true;
 
         if (null !== $sender) {
             $this->setFrom(...(array) $sender);
         }
         if (null !== $recipients) {
-            $this->addRecipients($recipients);
+            foreach ($recipients as $recipient) {
+                $this->addRecipient(...$recipient);
+            }
         }
         if (null !== $replies) {
-            $this->addReplies($replies);
+            foreach ($replies as $reply) {
+                $this->addReply(...$reply);
+            }
         }
-
-        $this->strict = $strict;
     }
 
     /**
@@ -94,36 +93,6 @@ final class SimpleMailer
     }
 
     /**
-     * Adds recipients.
-     *
-     * @param array<array{string, string|null}> $recipients
-     *
-     * @throws InvalidArgumentException
-     */
-    public function addRecipients(array $recipients): self
-    {
-        foreach ($recipients as $recipient) {
-            $this->addRecipient(...$recipient);
-        }
-
-        return $this;
-    }
-
-    /**
-     * Sets recipients.
-     *
-     * @param array<array{string, string|null}> $recipients
-     *
-     * @throws InvalidArgumentException
-     */
-    public function setRecipients(array $recipients = []): self
-    {
-        $this->mailer->clearAddresses();
-
-        return $this->addRecipients($recipients);
-    }
-
-    /**
      * Adds CC.
      *
      * @throws InvalidArgumentException
@@ -151,36 +120,6 @@ final class SimpleMailer
         $this->mailer->clearCCs();
 
         return $this->addCC($email, $name);
-    }
-
-    /**
-     * Adds CC's.
-     *
-     * @param array<array{string, string|null}> $copies
-     *
-     * @throws InvalidArgumentException
-     */
-    public function addCCs(array $copies): self
-    {
-        foreach ($copies as $copy) {
-            $this->addCC(...$copy);
-        }
-
-        return $this;
-    }
-
-    /**
-     * Sets CC's.
-     *
-     * @param array<array{string, string|null}> $copies
-     *
-     * @throws InvalidArgumentException
-     */
-    public function setCCs(array $copies = []): self
-    {
-        $this->mailer->clearCCs();
-
-        return $this->addCCs($copies);
     }
 
     /**
@@ -214,36 +153,6 @@ final class SimpleMailer
     }
 
     /**
-     * Adds replies.
-     *
-     * @param array<array{string, string|null}> $replies
-     *
-     * @throws InvalidArgumentException
-     */
-    public function addReplies(array $replies): self
-    {
-        foreach ($replies as $reply) {
-            $this->addReply(...$reply);
-        }
-
-        return $this;
-    }
-
-    /**
-     * Sets replies.
-     *
-     * @param array<array{string, string|null}> $replies
-     *
-     * @throws InvalidArgumentException
-     */
-    public function setReplies(array $replies = []): self
-    {
-        $this->mailer->clearReplyTos();
-
-        return $this->addReplies($replies);
-    }
-
-    /**
      * Adds custom header.
      *
      * @throws InvalidArgumentException
@@ -269,36 +178,6 @@ final class SimpleMailer
         $this->mailer->clearCustomHeaders();
 
         return $this->addCustomHeader($name, $value);
-    }
-
-    /**
-     * Adds custom headers.
-     *
-     * @param array<array{string, string|null}> $headers
-     *
-     * @throws InvalidArgumentException
-     */
-    public function addCustomHeaders(array $headers): self
-    {
-        foreach ($headers as $header) {
-            $this->addCustomHeader(...$header);
-        }
-
-        return $this;
-    }
-
-    /**
-     * Sets custom headers.
-     *
-     * @param array<array{string, string|null}> $headers
-     *
-     * @throws InvalidArgumentException
-     */
-    public function setCustomHeaders(array $headers = []): self
-    {
-        $this->mailer->clearCustomHeaders();
-
-        return $this->addCustomHeaders($headers);
     }
 
     /**
@@ -360,36 +239,6 @@ final class SimpleMailer
     }
 
     /**
-     * Adds attachment files.
-     *
-     * @param array<array{string, string|null, string|null}> $attachments
-     *
-     * @throws InvalidArgumentException
-     */
-    public function addAttachmentFiles(array $attachments): self
-    {
-        foreach ($attachments as $attachment) {
-            $this->addAttachmentFile(...$attachment);
-        }
-
-        return $this;
-    }
-
-    /**
-     * Sets attachment files.
-     *
-     * @param array<array{string, string|null, string|null}> $attachments
-     *
-     * @throws InvalidArgumentException
-     */
-    public function setAttachmentFiles(array $attachments = []): self
-    {
-        $this->mailer->clearAttachments();
-
-        return $this->addAttachmentFiles($attachments);
-    }
-
-    /**
      * Adds attachment string as file.
      *
      * @throws InvalidArgumentException
@@ -418,36 +267,6 @@ final class SimpleMailer
     }
 
     /**
-     * Adds attachment strings as files.
-     *
-     * @param array<array{string, string, string|null}> $attachments
-     *
-     * @throws InvalidArgumentException
-     */
-    public function addAttachmentStrings(array $attachments): self
-    {
-        foreach ($attachments as $attachment) {
-            $this->addAttachmentString(...$attachment);
-        }
-
-        return $this;
-    }
-
-    /**
-     * Sets attachment strings as files.
-     *
-     * @param array<array{string, string, string|null}> $attachments
-     *
-     * @throws InvalidArgumentException
-     */
-    public function setAttachmentStrings(array $attachments): self
-    {
-        $this->mailer->clearAttachments();
-
-        return $this->addAttachmentStrings($attachments);
-    }
-
-    /**
      * Creates message and sends it.
      *
      * @throws InvalidArgumentException
@@ -455,7 +274,7 @@ final class SimpleMailer
      */
     public function send(): bool
     {
-        if (empty($this->mailer->getToAddresses())) {
+        if (count($this->mailer->getToAddresses()) === 0) {
             if ($this->strict) {
                 throw new InvalidArgumentException('Must be at least one recipient');
             }
